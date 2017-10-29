efc362a83773bf7c8d4633efdaf7ee9c64ab8bce
ME_Kun_Han <hanvskun@hotmail.com>
2017-10-29 19:31:38 +0800

    add flvjs into pages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import 'flv.js';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+
+  ngOnInit(): void {
+    if (flvjs.isSupported()) {
+      const videoElement = <HTMLAudioElement>document.getElementById('videoElement');
+      const flvPlayer = flvjs.createPlayer({
+        type: 'flv',
+        url: 'http://192.168.1.250:8080/live/livestream.flv'
+      });
+      flvPlayer.attachMediaElement(videoElement);
+      flvPlayer.load();
+      flvPlayer.play();
+    }
+  }
 }
